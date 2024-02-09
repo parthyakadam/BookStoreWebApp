79b3471e7c9a1b937fc93e3a506a7796d78a50df
ParthKadam <ParthKadam@FMR-4019-PARTHK>
2024-02-09 15:32:53 +0530

    Section 9: Video 129- Completed Company CRUD

--- Bulky.Models/ApplicationUser.cs
+++ Bulky.Models/ApplicationUser.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +12,7 @@ namespace Bulky.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        //here the ApplicationUser now has all the properties of IdentityUser and here we can now add more properties that we require
+        //here the ApplicationUser now has all the properties of IdentityUser and here we can now add more properties that we require. We'll be able to identify the user like Customer, Company form below properties
 
         [Required]
         public String Name { get; set; }
@@ -22,5 +24,11 @@ namespace Bulky.Models
         public string? State { get; set; }
 
         public string? PostalCode { get; set; }
+
+        public int? CompanyId { get; set; }
+        [ForeignKey("CompanyId")]
+        [ValidateNever]
+        public Company Company { get; set; }
+        //above foreign key relation is added to check if the added user is Company user or not. this data won't be populated on view
     }
 }
