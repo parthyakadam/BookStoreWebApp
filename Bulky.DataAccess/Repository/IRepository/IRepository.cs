21aa84932e3966ae9f635ee92603fc08879233e7
ParthKadam <ParthKadam@FMR-4019-PARTHK>
2024-02-12 14:13:23 +0530

    Section 10: Video 141: Implemented dynamic shopping cart

--- Bulky.DataAccess/Repository/IRepository/IRepository.cs
+++ Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -8,8 +8,8 @@ using System.Threading.Tasks;
 namespace Bulky.DataAccess.Repository.IRepository
 {
     public interface IRepository<T> where T : class
-    { 
-        IEnumerable<T> GetAll(string? includeProperties = null);
+    {
+        IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null);
 
         T Get(Expression<Func<T, bool>> filter, string? includeProperties = null);
 
